d60c5ebd57a715a22d79c6090f272124834bff00
ArmanAstud <info.a.95@gmail.com>
2023-03-12 19:19:54 +0100

    Adding yaw angle, new HTML Divs and README updated

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,5 +1,5 @@
 import numpy as np
-import os, sys
+import os, sys, math
 import pandas as pd
 
 import dash
@@ -11,40 +11,76 @@ from dash.dependencies import Input, Output
 
 import plotly.graph_objs as go
 
+
+class Obstacle():
+	def __init__(self, df):
+		self.df = df
+
+		self.id = df['id']
+		
+		self.x_center	= df['x']
+		self.y_center	= df['y']
+		self.z_center	= df['z']
+		self.yaw		= df['yaw']
+
+		self.w 	= df['w']
+		self.l 	= df['l']
+		self.h 	= df['h']
+
+		self.score = df['score']
+		self.label = df['label']
+
+	def print_obstacle(self):
+		print('------')
+		print(self.df)
+		print('------\n')
+		
+
 def return_vertex(df, grid_x_min, grid_x_max, grid_y_min, grid_y_max):
 	all_vertex = []
 	all_labels = []
+	all_obstacles = []
+	#print(df)
 	for i in range(len(df)):
-		id_box	 = df['id'][i]
-		
-		x_center = df['x'][i]
-		y_center = df['y'][i]
-		z_center = df['z'][i]
+		# Parser obstacle
+		obstacle = Obstacle(df.iloc[int(i)])
+		obstacle.print_obstacle()
 
-		w_half 	= df['w'][i] / 2
-		l_half 	= df['l'][i] / 2
-		h 		= df['h'][i]
-		
+		id_box	 = obstacle.id
+		x_center = obstacle.x_center
+		y_center = obstacle.y_center
+		z_center = obstacle.z_center
+		yaw		 = obstacle.yaw
+		w_half 	= obstacle.w / 2
+		l_half 	= obstacle.l / 2
+		h 		= obstacle.h
+
 		# Construir vertices 
-		x_min = x_center - l_half
-		y_min = y_center - w_half
-		z_min = z_center
-		x_max = x_center + l_half
-		y_max = y_center + w_half
-		z_max = z_center + h
+		point_A_x = (x_center - l_half * math.cos(-yaw) - w_half  * math.sin(-yaw))
+		point_A_y = (y_center + l_half * math.sin(-yaw) - w_half  * math.cos(-yaw))
 
-		if x_min<grid_x_min or x_max>grid_x_max or y_min<grid_y_min or y_max>grid_y_max: continue
+		# Get B point
+		point_B_x = (x_center + l_half* math.cos(-yaw) - w_half  * math.sin(-yaw))
+		point_B_y = (y_center - l_half* math.sin(-yaw) - w_half  * math.cos(-yaw))
+
+		# Get C point
+		point_C_x = (x_center + l_half * math.cos(-yaw) + w_half  * math.sin(-yaw))
+		point_C_y = (y_center - l_half * math.sin(-yaw) + w_half  * math.cos(-yaw))
+
+		# Get D point
+		point_D_x = (x_center - l_half * math.cos(-yaw) + w_half  * math.sin(-yaw))
+		point_D_y = (y_center + l_half * math.sin(-yaw) + w_half  * math.cos(-yaw))
 		
 		vertices = np.array([
-			[x_min, y_min, z_min],
-			[x_max, y_min, z_min],
-			[x_max, y_max, z_min],
-			[x_min, y_max, z_min],
+			[point_A_x, point_A_y, z_center],
+			[point_B_x, point_B_y, z_center],
+			[point_C_x, point_C_y, z_center],
+			[point_D_x, point_D_y, z_center],
 
-			[x_min, y_min, z_max],
-			[x_max, y_min, z_max],
-			[x_max, y_max, z_max],
-			[x_min, y_max, z_max]
+			[point_A_x, point_A_y, z_center+h],
+			[point_B_x, point_B_y, z_center+h],
+			[point_C_x, point_C_y, z_center+h],
+			[point_D_x, point_D_y, z_center+h]
 		])
 		indices = np.array([
 			[0, 1, 2, 3],
@@ -56,7 +92,7 @@ def return_vertex(df, grid_x_min, grid_x_max, grid_y_min, grid_y_max):
 		])
 
 		all_vertex.append(vertices)
-		all_labels.append('Car: {}'.format(id_box))
+		all_labels.append('{}-{}: {:.3f}'.format(df['label'][i], id_box, df['score'][i]))
 
 	return all_vertex, all_labels
 
@@ -74,9 +110,6 @@ def draw_frame(data_dir, frame_list, frame, fig, grid_x_min, grid_x_max, grid_y_
 			x=vertices[:, 0],
 			y=vertices[:, 1],
 			z=vertices[:, 2],
-			#i=indices[:, 0],
-			#j=indices[:, 1],
-			#k=indices[:, 2],
 			i = [7, 0, 0, 0, 4, 4, 6, 6, 4, 0, 3, 2],
 			j = [3, 4, 1, 2, 5, 6, 5, 2, 0, 1, 6, 3],
 			k = [0, 7, 2, 3, 6, 7, 1, 1, 5, 5, 7, 6],
